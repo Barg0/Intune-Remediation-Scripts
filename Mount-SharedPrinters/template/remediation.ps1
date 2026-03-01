2f5f7ad09f589d7ec3a11be9d1a140eb71c44d95
Barg0 <89658787+Barg0@users.noreply.github.com>
2026-03-01 11:18:42 +0100

    Refactor logging and printer management in script

--- Mount-SharedPrinters/template/remediation.ps1
+++ Mount-SharedPrinters/template/remediation.ps1
@@ -17,7 +17,7 @@ $logGet        = $true
 $logRun        = $true
 $enableLogFile = $true
 
-$logFileDirectory = "$env:ProgramData\IntuneLogs\Scripts\$($env:USERNAME)\$scriptName"
+$LogFileDirectory = "$env:ProgramData\IntuneLogs\Scripts\$env:USERNAME\$scriptName"
 $logFile          = "$logFileDirectory\$logFileName"
 
 if ($enableLogFile -and -not (Test-Path -Path $logFileDirectory)) {
@@ -146,4 +146,5 @@ else {
     Write-Log "$($missingPrinters.Count) printer(s) are still missing." -Tag "Error"
     Complete-Script -ExitCode 1
 }
+
 
